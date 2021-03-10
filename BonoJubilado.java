e088291c1053fc0d33ccdccc20e0864b1bbac79a
Álvaro <alvaro.jimenez.medina.84@gmail.com>
2021-03-10 10:41:48 +0100

    Clases básicas creadas

---
+++ BonoJubilado.java
@@ -0,0 +1,22 @@
+package pacalcompany.bonobus;
+/**
+ * Clase BonoBus para jubilados
+ * @author Álvaro
+ * @see BonoBus
+ * @see BonoReducido
+ */
+public class BonoJubilado extends BonoReducido{
+
+    /**
+     * Constructor.
+     * @param DNI
+     * @param nombre
+     * @param apellidos
+     * @param edad 
+     */
+    public BonoJubilado(String DNI, String nombre, String apellidos, int edad){
+        super(DNI, nombre, apellidos, edad);
+        
+    }
+    
+}
